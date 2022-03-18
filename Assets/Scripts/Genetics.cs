2762484ef31b670e29a41212750d97404a08ca0a
Amaan Miah <miaha21@coventry.ac.uk>
2022-03-18 10:54:29 +0000

    Still working on Genetic inheritance

--- Assets/Scripts/Genetics.cs
+++ Assets/Scripts/Genetics.cs
@@ -36,7 +36,7 @@ public enum GENOME
 public class Genetics
 {
     // Readonly dictionary used as a lookup table that supports multiple types
-    static IReadOnlyDictionary<GENOME, DnaConstants> genome_Values = null;
+    static IReadOnlyDictionary<GENOME, GenomeConstant> genome_Values = null;
 
     // Attribute genes
     GeneFactor<float> athleticism;
@@ -58,12 +58,12 @@ public class Genetics
         }
 
         // get value assigned to genome from lookup table
-        GenerateRandomFactor(out athleticism, 1, 1, 0.05f, GENOME.ATHLETICISM);
-        GenerateRandomFactor(out offense, 1, 1, 0.05f, GENOME.OFFENSE);
-        GenerateRandomFactor(out defense, 1, 1, 0.05f, GENOME.DEFENSE);
-        GenerateRandomFactor(out size, 1, 1, 0.05f, GENOME.SIZE);
+        athleticism = GenerateRandomFactor<float>(GENOME.ATHLETICISM);
+        offense = GenerateRandomFactor<float>(GENOME.OFFENSE);
+        defense = GenerateRandomFactor<float>(GENOME.DEFENSE);
+        size = GenerateRandomFactor<float>(GENOME.SIZE);
 
-        GenerateRandomFactor(out agression, 1, 1, 0.05f, GENOME.AGRESSION);
+        agression = GenerateRandomFactor<float>(GENOME.AGRESSION);
     }
 
     // Generate genes based on parents (limits mutation chance and mutation range)
@@ -76,53 +76,110 @@ public class Genetics
 
         // Use parents genes to determine mutation rate, the range of mutation if successfull and the starting gene
         if (parents != null) {
-            GenerateFactorsFromGenetics(parents, 2, 0.8f);
+            GenerateFactorFromGenetics(parents, 2, 0.8f, athleticism, GENOME.ATHLETICISM);
+            GenerateFactorFromGenetics(parents, 2, 0.8f, offense, GENOME.OFFENSE);
+            GenerateFactorFromGenetics(parents, 2, 0.8f, defense, GENOME.DEFENSE);
+            GenerateFactorFromGenetics(parents, 2, 0.8f, size, GENOME.SIZE);
+
+            GenerateFactorFromGenetics(parents, 2, 0.8f, agression, GENOME.AGRESSION);
         }
 
         // Use grandparents' genes to determine mutation 
         if (grandParents != null) {
-            GenerateFactorsFromGenetics(grandParents, 4, 0.2f);
+            GenerateFactorFromGenetics(grandParents, 4, 0.2f, athleticism, GENOME.ATHLETICISM);
+            GenerateFactorFromGenetics(grandParents, 4, 0.2f, offense, GENOME.OFFENSE);
+            GenerateFactorFromGenetics(grandParents, 4, 0.2f, defense, GENOME.DEFENSE);
+            GenerateFactorFromGenetics(grandParents, 4, 0.2f, size, GENOME.SIZE);
+
+            GenerateFactorFromGenetics(grandParents, 4, 0.2f, agression, GENOME.AGRESSION);
         }
 
         
     }
 
-    bool GenerateRandomFactor<T>(out GeneFactor<T> factor, int highRange, int lowRange, float mutationRate, GENOME genome)
+    GeneFactor<T> GenerateRandomFactor<T>(GENOME genome)
     {
-        int dnaSelection = UnityEngine.Random.Range(0, (int)DNA.NUM_DNA - 1);
-        T[] values = (T[]) genome_Values[genome].dnaValuesArray;
+        GenomeConstant genomeConstant = genome_Values[genome];
 
+        int dnaSelection = UnityEngine.Random.Range(0, (int)DNA.NUM_DNA - 1);
+
+        T[] values = (T[]) genomeConstant.dnaValuesArray;
         T val = values[dnaSelection];
         // Add variance to floating point values by +-10% 
         if (typeof(T) == typeof(float)) {
             val = (T)(object) ((float)(object)val * Random.Range(0.9f, 1.1f));
         }
 
-        factor = new GeneFactor<T>(highRange, lowRange, mutationRate, val, (DNA) dnaSelection);
-        return true;
+        return new GeneFactor<T>(val, (DNA) dnaSelection);
 
     }
 
-    void GenerateFactorsFromGenetics(Genetics[] genes, int length, float influence) {
+    void GenerateFactorFromGenetics<T>(Genetics[] genes, int length, float influence, GeneFactor<T> factor, GENOME genome) {
         // Check if factors have already been created, if so then alter genes rather than generate new ones
+        GenomeConstant genomeConstant = genome_Values[genome];
+        T[] dnaValues = (T[]) genomeConstant.dnaValuesArray;
 
-        if (athleticism == null) athleticism = new GeneFactor<float>(0,0,0f, 0f, DNA.POSITIVE);
+        if (factor == null) factor = new GeneFactor<T>(dnaValues[(int) DNA.NEUTRAL], DNA.NEUTRAL);
 
-        for (int i = 0; i < length; i++) {
+        int[] randomRangeList = new int[4];
+
+        for (int i = 0; i < length; i+=2) {
             // Determine mutation rate higher and lower
-            
+
+            DNA parent1Dna = genes[0].athleticism.dna;
+            DNA parent2Dna = genes[1].athleticism.dna;
+            DNA myDna = factor.dna;
+            float avg = ((float)parent1Dna + (float)parent2Dna) / 2f;
         }
     }
 
-    Dictionary<GENOME, DnaConstants> CreateGenomeValueLookup() {
-        // Create constant default values for the genomes for each DNA a single time
-        var temp = new Dictionary<GENOME, DnaConstants>((int)GENOME.NUM_GENOMES);
+    void GenerateAllFactorsFromGenetics(Genetics[] genes, int length, float influence) {
+        // Check if factors have already been created, if so then alter genes rather than generate new ones
+        GenomeConstant genomeConstant = genome_Values[GENOME.ATHLETICISM];
+        float[] dnaValues = (float[])genomeConstant.dnaValuesArray;
 
-        temp.Add(GENOME.ATHLETICISM, new DnaConstants(new float[]   { 0.5f, 0.8f, 1f, 1.18f, 1.4f }, 0.1f, 1, 1));
-        temp.Add(GENOME.OFFENSE, new DnaConstants(new float[]       { 5f, 10f, 15f, 23f, 30f }, 0.3f, 2, 2));
-        temp.Add(GENOME.DEFENSE, new DnaConstants(new float[]       { 7f, 10f, 13f, 16f, 20f }, 0.3f, 2, 2));
-        temp.Add(GENOME.SIZE, new DnaConstants(new float[]          { 0.1f, 0.3f, 0.5f, 0.8f, 1.2f }, 0.2f, 3, 3));
-        temp.Add(GENOME.AGRESSION, new DnaConstants(new float[]     { 0.05f, 0.2f, 0.4f, 0.7f, 0.9f }, 0.5f, 4, 4));
+        if (athleticism == null) athleticism = new GeneFactor<float>(dnaValues[(int)DNA.NEUTRAL], DNA.NEUTRAL);
+
+        int[] randomRangeList = new int[100];
+        int itemsToFillFromParents = (int) (influence * randomRangeList.Length);
+
+        DNA parent1Dna = genes[0].athleticism.dna;
+        DNA parent2Dna = genes[1].athleticism.dna;
+        DNA myDna = athleticism.dna;
+
+        // Determine starting gene by selecting randomly from this weighted list
+        for (int i = 0; i < itemsToFillFromParents; i+=2)
+        {
+            randomRangeList[i] = (int)parent1Dna;
+            randomRangeList[i + 1] = (int)parent2Dna;
+        }
+
+        for (int i = itemsToFillFromParents; i < randomRangeList.Length; i++)
+        {
+            randomRangeList[i] = (int)myDna;
+        }
+
+        // Choose gene from list randomly
+        DNA randomDna = (DNA) Random.Range(0, randomRangeList.Length);
+
+        for (int i = 0; i < length; i += 2)
+        {
+
+            // Determine mutation rate higher and lower
+
+        }
+
+    }
+
+    Dictionary<GENOME, GenomeConstant> CreateGenomeValueLookup() {
+        // Create constant default values for the genomes for each DNA a single time
+        var temp = new Dictionary<GENOME, GenomeConstant>((int)GENOME.NUM_GENOMES);
+
+        temp[GENOME.ATHLETICISM] = new GenomeConstant(new float[]   { 0.5f, 0.8f, 1f, 1.18f, 1.4f }, 0.1f, 1, 1);
+        temp[GENOME.OFFENSE] = new GenomeConstant(new float[]       { 5f, 10f, 15f, 23f, 30f }, 0.3f, 2, 2);
+        temp[GENOME.DEFENSE] = new GenomeConstant(new float[]       { 7f, 10f, 13f, 16f, 20f }, 0.3f, 2, 2);
+        temp[GENOME.SIZE] = new GenomeConstant(new float[]          { 0.1f, 0.3f, 0.5f, 0.8f, 1.2f }, 0.2f, 3, 3);
+        temp[GENOME.AGRESSION] = new GenomeConstant(new float[]     { 0.05f, 0.2f, 0.4f, 0.7f, 0.9f }, 0.5f, 4, 4);
 
         return temp;
     }
@@ -134,33 +191,26 @@ public class GeneFactor<T>
     public readonly T value;
     public readonly DNA dna;
 
-    public int mutationRange_higher;
-    public int mutationRange_lower;
-    public float mutationRate;
-
     public float experience; // Affects the +-chance of mutation for the next generation as well as mutation range
 
-    public GeneFactor(int mutationHigher, int mutationLower, float baseMutationRate, T value, DNA dna)
+    public GeneFactor(T value, DNA dna)
     {
         this.value = value;
         this.dna = dna;
-        mutationRange_higher = mutationHigher;
-        mutationRange_lower = mutationLower;
-        mutationRate = baseMutationRate;
 
         experience = 0f;
     }
 
 }
 
-readonly public struct DnaConstants
+readonly public struct GenomeConstant
 {
-    public object dnaValuesArray { get; } // An array of any type i.e. 
+    public object dnaValuesArray { get; } // An array of any type i.e. float[] (cast object -> T[])
     public float mutationRateBase {get; } // Volatility of the gene (Frequency of change between generations)
     public int mutationRange_higher {get;} // How high the gene can mutate to a better version
     public int mutationRange_lower {get;} // How low the gene can mutate to a worse version
 
-    public DnaConstants(object arrayConstants, float mutationRate, int mutationRangeHigh, int mutationRangeLow) {
+    public GenomeConstant(object arrayConstants, float mutationRate, int mutationRangeHigh, int mutationRangeLow) {
         dnaValuesArray = arrayConstants;
         mutationRateBase = mutationRate;
         mutationRange_higher = mutationRangeHigh;
